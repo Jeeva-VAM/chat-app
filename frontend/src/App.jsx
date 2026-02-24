13a66707f2e069103625383ff0125dda1bacd722
Haaripriya <haaripriya.anandhakumar@valuemomentum.in>
2026-02-24 16:27:28 +0530

    login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,12 @@
-import { useState } from 'react'
 import './App.css'
+import Login from './Pages/LoginPage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <Login />
     
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-      </div>
      
     </>
   )
