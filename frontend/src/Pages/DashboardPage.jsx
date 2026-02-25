92fed5e78032b68b1356b622d4e6e586b0a5c5ae
Jeeva-VAM <Jeeva.Krishnamoorthy@valuemomentum.com>
2026-02-25 13:01:18 +0530

    navbar,profileIcon,filled dashboard along with css files

--- frontend/src/Pages/DashboardPage.jsx
+++ frontend/src/Pages/DashboardPage.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+import Navbar from "../components/Navbar";
 
 function DashboardPage() {
+  const [user] = useState(() => {
+    const userDetails = localStorage.getItem("user");
+    return userDetails ? JSON.parse(userDetails) : null;
+  });
+
+  if (!user) return null;
+
   return (
-    <div>
-      <h1>Welcome to Chat App</h1>
-        
+    <div className="dashboard">
+      <Navbar />
+
+      <div className="dashboard-content">
+        <h1>Welcome {user.name} </h1>
+        <p>Select a chat to start messaging</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default DashboardPage
+export default DashboardPage;
--- frontend/src/components/ProfileIcon.jsx
+++ frontend/src/components/ProfileIcon.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/ProfileIcon.css";
 
 function ProfileIcon() {
+  const navigate = useNavigate();
+
+  const [user] = useState(() => {
+    const userDetails = localStorage.getItem("user");
+    return userDetails ? JSON.parse(userDetails) : null;
+  });
+
+
+//   useEffect(()=>{
+    
+//   },[])
+
+  if (!user) return null;
+
   return (
-    <div>
-      
+    <div className="profile-wrapper">
+      <div
+        className="profile-avatar"
+        onClick={() => navigate("/profile")}
+      >
+        {user?.picture ? (
+          <img src={user.picture} alt="avatar" />
+        ) : (
+          <span>{user?.name?.charAt(0).toUpperCase()}</span>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProfileIcon
+export default ProfileIcon;
