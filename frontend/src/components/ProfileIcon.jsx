bf5c24f3011825c073e4a32430bedfb9ea7f2d50
Haaripriya <haaripriya.anandhakumar@valuemomentum.in>
2026-02-25 11:32:48 +0530

    beautify

--- frontend/src/components/ProfileIcon.jsx
+++ frontend/src/components/ProfileIcon.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function ProfileIcon() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
 
-export default ProfileIcon
+export default ProfileIcon;
