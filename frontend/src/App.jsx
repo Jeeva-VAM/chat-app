c5305a17b56989eb0f102e8ec5a6baa0061df7be
Haaripriya <haaripriya.anandhakumar@valuemomentum.in>
2026-02-25 11:32:48 +0530

    beautify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,17 @@
-
-import './App.css'
-import DashboardPage from './pages/DashboardPage'
-import Login from './Pages/LoginPage'
+import "./App.css";
+import DashboardPage from "./pages/DashboardPage";
+import Login from "./Pages/LoginPage";
 import { Routes, Route } from "react-router-dom";
 
-
 function App() {
-
   return (
     <>
-<Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-    </Routes>
-     
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
