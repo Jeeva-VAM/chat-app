2bb50066d270f3029613629f89babca012802f9f
Haaripriya <haaripriya.anandhakumar@valuemomentum.in>
2026-02-26 11:57:19 +0530

    navbar

--- frontend/src/Pages/DashboardPage.jsx
+++ frontend/src/Pages/DashboardPage.jsx
@@ -1,24 +1,11 @@
-import { useState } from "react";
-import Navbar from "../components/Navbar";
-
 function DashboardPage() {
-  const [user] = useState(() => {
-    const userDetails = localStorage.getItem("user");
-    return userDetails ? JSON.parse(userDetails) : null;
-  });
-
-  if (!user) return null;
-
+  
   return (
-    <div className="dashboard">
-      <Navbar />
-
-      <div className="dashboard-content">
-        <h1>Welcome {user.name} </h1>
-        <p>Select a chat to start messaging</p>
-      </div>
+    <div>
+      <h1>Welcome to Chat App</h1>
+        
     </div>
-  );
+  )
 }
 
-export default DashboardPage;
+export default DashboardPage
