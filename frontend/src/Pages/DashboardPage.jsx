3d68c3fbbfef7fb4e3008e90374111ad516077de
Jeeva-VAM <Jeeva.Krishnamoorthy@valuemomentum.com>
2026-02-26 15:02:00 +0530

    removed duplicate navbar in dashboard , impported json as user in dashboard

--- frontend/src/Pages/DashboardPage.jsx
+++ frontend/src/Pages/DashboardPage.jsx
@@ -2,12 +2,11 @@ import { useState } from "react";
 import Navbar from "../components/Navbar";
 import Suggestions from "../components/suggestions";
 import SuggestionsRow from "../components/SuggestionsRow";
-
+import user from '../data/users.json'
 function DashboardPage() {
   
   return (
     <div className="dashboard">
-      <Navbar />
 
       <div className="dashboard-content">
         <h1>Welcome {user.name} </h1>
